19fee77e094464edc3e2f9ccec9bdc27b40fa615
shi-man <aya.a.shimada@gmail.com>
2019-05-12 11:34:51 +0000

    Add test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,6 +1,7 @@
 import unittest
 import sys
 import json
+
 try:
     from StringIO import StringIO
 except ImportError:
@@ -20,25 +21,63 @@ class TestSample(unittest.TestCase):
     def test_sample(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
+    def __data_provider(self):
+        return {
+            'default input': {
+                'input':
+                    "\n" +  # Please enter title [""]:
+                    "\n" +  # Please enter version [v1.0]:
+                    "\n" +  # Please enter license [Apache 2.0]:
+                    "\n" +  # Please enter server url [http://example.com]:
+                    "/users\n" +  # Please enter path [/]:
+                    "get\n" +  # Please enter method for /users [get|post|put|delete|head|option|trace]
+                    "\n" +  # Please enter description for get /users [""]:
+                    "\n" +  # Please enter response code for get /users [200]:
+                    "succeed to return all users info\n" +  # Please enter response description for 200:
+                    "application/json\n" +  # Please enter response content-type for get /users [application/json]:
+                    "\n" +  # Add more response for get /users ? Y/n [n]:
+                    "\n",  # Add more request method for /users ? Y/n [n]:
+                'expected': 'tests/resources/expected-default-output.json'
+            },
+            'multiple response input': {
+                'input':
+                    "\n" +  # Please enter title [""]:
+                    "\n" +  # Please enter version [v1.0]:
+                    "\n" +  # Please enter license [Apache 2.0]:
+                    "\n" +  # Please enter server url [http://example.com]:
+                    "/users\n" +  # Please enter path [/]:
+                    "get\n" +  # Please enter method for /users [get|post|put|delete|head|option|trace]
+                    "\n" +  # Please enter description for get /users [""]:
+                    "\n" +  # Please enter response code for get /users [200]:
+                    "succeed to return all users info\n" +  # Please enter response description for 200:
+                    "application/json\n" +  # Please enter response content-type for get /users [application/json]:
+                    "Y\n" +  # Add more response for get /users ? Y/n [n]:
+                    "400\n" +  # Please enter response code for get /users [200]:
+                    "failed to return all users infon\n" +  # Please enter response description for 200:
+                    "application/json\n" +  # Please enter response content-type for get /users [application/json]:
+                    "\n" +  # Add more response for get /users ? Y/n [n]:
+                    "Y\n" +  # Add more request method for /users ? Y/n [n]:
+                    "post\n" +  # Please enter method for /users [get|post|put|delete|head|option|trace]
+                    "\n" +  # Please enter description for post /users [""]:
+                    "\n" +  # Please enter response code for post /users [200]:
+                    "succeed to register a new user\n" +  # Please enter response description for 200:
+                    "application/json\n" +  # Please enter response content-type for post /users [application/json]:
+                    "Y\n" +  # Add more response for %s %s ? Y/n [n]:
+                    "400\n" +  # Please enter response code for get /users [200]:
+                    "failed to register a new user\n" +  # Please enter response description for 200:
+                    "application/json\n" +  # Please enter response content-type for post /users [application/json]:
+                    "\n" +  # Add more response for post /users ? Y/n [n]:
+                    "\n",  # Add more request method for /users ? Y/n [n]:
+                'expected': 'tests/resources/expected-multiple-response-output.json'
+            }
+        }
+
     def test_scaffold(self):
         original_sysin = sys.stdin
-        sys.stdin = StringIO(
-            "\n" +  # Please enter title [""]:
-            "\n" +  # Please enter version [v1.0]:
-            "\n" +  # Please enter license [Apache 2.0]:
-            "\n" +  # Please enter server url [http://example.com]:
-            "/users\n" +  # Please enter path [/]:
-            "get\n" +  # Please enter method for /users [get|post|put|delete|head|option|trace]
-            "\n" +  # Please enter description for get /users [""]:
-            "\n" +  # Please enter response code for get \users [200]:
-            "succeed to register a new user\n" +  # Please enter response description for 200:
-            "application/json\n" +  # Please enter response content-type for get /users [application/json]:
-            "\n" +  # Add more response for %s %s ? Y/n [n]:
-            "\n"  # Add more request method for %s ? Y/n [n]:
-        )
-        acutual = scaffold()
+        for key, value in self.__data_provider().items():
+            sys.stdin = StringIO(value['input'])
+            acutual = scaffold()
+            expected_str = read_file(value['expected'])
+            expected = json.loads(expected_str)
+            self.assertEqual(acutual, expected, key)
         sys.stdin = original_sysin
-        expected_str = read_file('tests/resources/expected-default-output.json')
-        expected = json.loads(expected_str)
-
-        self.assertEqual(acutual, expected, 'default input')
