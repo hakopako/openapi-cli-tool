a5b981d6b5d432a2e6583adc77097a6ac3ae39dd
Ayaka Shimada <aya.a.shimada@gmail.com>
2019-06-01 17:29:17 +0200

    Opt cli lib (#4)
    * cli lib docopt -> click
    
    * fix readme

--- src/main.py
+++ src/main.py
@@ -1,28 +1,27 @@
-"""OpenAPI CLI Tool
-
-Usage:
-  openapi-cli-tool list <spec-path>
-  openapi-cli-tool resolve <method> <path> <spec-path>
-  openapi-cli-tool scaffold
-
-Options:
-  -h --help     Show help.
-  --version     Show version.
-
-"""
-from docopt import docopt
+import click
 from src.commands.scaffold import scaffold
 from src.commands.list import list
 from src.commands.resolve import resolve
 
+@click.group()
 def main():
-    arguments = docopt(__doc__, version='OpenAPI CLI Tool 0.1')
-    if arguments['scaffold']:
-        scaffold()
-    elif arguments['list']:
-        list(arguments['<spec-path>'])
-    elif arguments['resolve']:
-        resolve(arguments['<method>'], arguments['<path>'], arguments['<spec-path>'])
+    pass
+
+@main.command('list', help='List up APIs in a specific file or directory.')
+@click.argument('file_path', type=click.Path(exists=True))
+def cmd_list(file_path):
+    list(file_path)
+
+@main.command('resolve', help='Display `$ref` resolved API specification.')
+@click.argument('method')
+@click.argument('path')
+@click.argument('file_path')
+def cmd_resolve(method, path, file_path):
+    resolve(method, path, file_path)
+
+@main.command('scaffold', help='Interactively create a simple OpenAPI Specification.')
+def cmd_scaffold():
+    scaffold()
 
 if __name__ == '__main__':
     main()
