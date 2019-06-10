445dc3ff6a534f7363005ac94e40cba84cb5c00e
shi-man <aya.a.shimada@gmail.com>
2019-06-10 11:04:09 +0200

    add yaml export option

--- src/utils/export.py
+++ src/utils/export.py
@@ -1,9 +1,15 @@
 import json
+import yaml
 from tabulate import tabulate
 
 
 def export_json(export_data):
     result = json.dumps(export_data, indent=2, ensure_ascii=False)
+    print(result)
+
+
+def export_yaml(export_data):
+    result = yaml.safe_dump(export_data, default_flow_style=False)
     print(result)
 
 
